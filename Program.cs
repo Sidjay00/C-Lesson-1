6f24bad590f5b92d70c9e15bf6efa98cf7cb7b38
K.Sidorov <2887458@gmail.com>
2023-02-16 22:21:56 +0300

    проверка равенства квадрата числа с числом

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int number, number2;
+Console.Write("Введите число: ");
+int.TryParse(Console.ReadLine()!, out number);
+Console.Write("Введите число: ");
+int.TryParse(Console.ReadLine()!, out number2);
+
+if (number == number2 * number2)
+{
+    Console.WriteLine("Да");
+}
+if (number != number2 * number2)
+{
+    Console.WriteLine("Нет");
+}
